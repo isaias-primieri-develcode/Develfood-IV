7bc6ea3a231784d64bbe59fb0fe870a4f78aa384
Isaias Primieri <isaiaspferreira@gmail.com>
2022-05-05 15:06:26 -0300

    ajustes

--- src/splash/index.tsx
+++ src/splash/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 import React from 'react';
 import LottieView from 'lottie-react-native'
+// eslint-disable-next-line import/no-unresolved
 import splashScreen from '../assets/splash/splashScreen.json'
 import { Container, Title } from './style';
 
